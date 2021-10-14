b661af0f163f7e8f85d2718dbebad146c1a543d0
max-wn <masaraksh@icloud.com>
2021-10-14 12:25:58 +0300

    initial commit
    initial commit

---
+++ program/main.py
@@ -0,0 +1,48 @@
+import pygame
+import pygame as pg
+
+w, h = 20, 20
+tile = 20
+clock = pg.time.Clock()
+
+screen = pg.display.set_mode((w * tile, h * tile))
+
+run = True
+
+map_x = 0
+map_y = 0
+
+map_lvl1 = [
+    [1, 1, 1, 0, 0, 0, ],
+    [1, 0, 0, 0, 0, 0, ],
+    [1, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 1, 0, 0, ],
+    [0, 0, 1, 1, 1, 0, ],
+    [0, 0, 0, 1, 0, 0, ],
+]
+
+
+def Map(map):
+    x = -w
+    y = -h
+    for i in map:
+        y += h
+        for j in i:
+            x += w
+            
+            if j == 1:
+
+                pg.draw.rect(screen, (255, 0, 0), (x, y, w, h), 1)
+            if i[j] == i[-1]:
+                x = -w
+
+
+while run:
+    for event in pg.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    # pg.draw.rect(screen, (0, 0, 255), (0, 0, 100, 100))
+    Map(map_lvl1)
+    pg.display.update()
+    clock.tick(60)
+pg.quit()
